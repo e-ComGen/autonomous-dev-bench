e873c44c87d9adff3ef43534edf02703ad1b7610
e-ComGen <kavodasskavo@gmail.com>
2026-09-09 05:17:52 +0400

    Export Phase 0-1 benchmark contracts

--- packages/benchmark_core/benchmark_core/__init__.py
+++ packages/benchmark_core/benchmark_core/__init__.py
@@ -6,6 +6,16 @@ normal production APIs and translate their outputs into these neutral records.
 
 from .bootstrap import EnvironmentBuildResult, ProjectEnvironmentBuilder
 from .experiment import ExperimentSpec, SuitePlan, SystemUnderTest
+from .experiment_manifest import (
+    AgentManifest,
+    BudgetManifest,
+    ExecutionManifest,
+    ExperimentManifest,
+    ModelManifest,
+    TaskManifest,
+    TrialManifest,
+    TrialOutputs,
+)
 from .identity import CanonicalModel, CommitPin, FrozenDict, Sha256Digest, VersionIdentity, canonical_json
 from .isolation import SandboxTrustStore
 from .manifest import load_project, load_scenario, load_suite, load_task
@@ -15,18 +25,26 @@ from .result import HardGate, OracleResult, RunResult, RunStatus, StageResult, S
 from .runner import BenchmarkSuite, ExperimentRunner, RunContext, SystemAdapter
 from .sandbox import DockerSandboxProvider
 from .scenario import ScenarioSpec
+from .swebench_v5 import OfficialSwebenchV5, SwebenchPrediction, require_v5, write_predictions
 from .task import TaskSpec
 
 __all__ = [
+    "AgentManifest",
     "BenchmarkSuite",
+    "BudgetManifest",
     "CanonicalModel",
     "CommitPin",
     "DockerSandboxProvider",
     "EnvironmentBuildResult",
+    "ExecutionManifest",
+    "ExperimentManifest",
     "ExperimentRunner",
     "ExperimentSpec",
     "FrozenDict",
     "HardGate",
+    "InvalidExperiment",
+    "ModelManifest",
+    "OfficialSwebenchV5",
     "OracleResult",
     "ProjectEnvironmentBuilder",
     "ProjectSpec",
@@ -34,23 +52,28 @@ __all__ = [
     "RunResult",
     "RunStatus",
     "SandboxTrustStore",
+    "ScenarioCheckpointMaterializer",
     "ScenarioSpec",
     "Sha256Digest",
     "StageResult",
     "SuitePlan",
     "SuiteResult",
+    "SwebenchPrediction",
     "SystemAdapter",
     "SystemObservation",
     "SystemUnderTest",
+    "TaskManifest",
     "TaskSpec",
+    "TrialManifest",
+    "TrialOutputs",
     "VersionIdentity",
     "canonical_json",
     "load_project",
     "load_scenario",
-    "load_task",
     "load_suite",
-    "InvalidExperiment",
-    "ScenarioCheckpointMaterializer",
+    "load_task",
+    "require_v5",
+    "write_predictions",
 ]
 
 __version__ = "0.1.0"
